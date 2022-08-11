26dfb4a479d36bed1b0b5ae2e2e7453efa993d8f
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-08-11 18:39:02 -0300

    Criação da caixa de Trenging Hashtags

--- src/Pages/Posts.js
+++ src/Pages/Posts.js
@@ -1,7 +1,21 @@
+import styled from "styled-components";
 import PostCreate from "../Components/Post/PostCreate";
+import Trending from "../Components/Trending/Trending";
 
 export default function Posts (){
   return (
-    <PostCreate/>
+    <FeedContainer>
+      <PostCreate/>
+      <Trending />
+    </FeedContainer>
   );
 }
+
+const FeedContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 25px;
+`
