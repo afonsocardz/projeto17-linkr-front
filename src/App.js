34d2ce645d4bf224e7a41ef26ce58a55a96062e3
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-08-11 08:09:31 -0300

    Iniciando tela de cadastro

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./Pages/SignUp/SignUp";
 
 export default function App() {
-    console.log("oi");
   return (
     <BrowserRouter>
       <Routes>
